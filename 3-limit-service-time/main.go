d1075ea50334f013b46a9844b16920ef72054b29
Antonov Denis <den_ru1982@mail.ru>
2024-01-02 22:17:45 +0300

    Solved 3-limit-service-time

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,11 @@
 
 package main
 
+import (
+	"context"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +26,24 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	chProcess := make(chan struct{})
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	go func() {
+		process()
+		close(chProcess)
+	}()
+	for {
+		select {
+		case <-chProcess:
+			return true
+		case <-ctx.Done():
+			if u.IsPremium {
+				return true
+			}
+			return false
+		}
+	}
 }
 
 func main() {
