f64973f593718be8f385b4da4b2903cf8534d0c1
Antonov Denis <den_ru1982@mail.ru>
2024-01-02 18:25:36 +0300

    Solve 2-race-in-cache

--- 2-race-in-cache/main.go
+++ 2-race-in-cache/main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"container/list"
+	"sync"
 	"testing"
 )
 
@@ -29,6 +30,7 @@ type page struct {
 
 // KeyStoreCache is a LRU cache for string key-value pairs
 type KeyStoreCache struct {
+	mx    sync.Mutex
 	cache map[string]*list.Element
 	pages list.List
 	load  func(string) string
@@ -37,6 +39,7 @@ type KeyStoreCache struct {
 // New creates a new KeyStoreCache
 func New(load KeyStoreCacheLoader) *KeyStoreCache {
 	return &KeyStoreCache{
+		mx:    sync.Mutex{},
 		load:  load.Load,
 		cache: make(map[string]*list.Element),
 	}
@@ -44,6 +47,8 @@ func New(load KeyStoreCacheLoader) *KeyStoreCache {
 
 // Get gets the key from cache, loads it from the source if needed
 func (k *KeyStoreCache) Get(key string) string {
+	k.mx.Lock()
+	defer k.mx.Unlock()
 	if e, ok := k.cache[key]; ok {
 		k.pages.MoveToFront(e)
 		return e.Value.(page).Value
